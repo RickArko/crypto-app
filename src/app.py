2a079c9c71f0e6490cb1f117c5f15b955ffc4aec
Rick Arko <rickarko@Ricks-MacBook-Pro.local>
2021-01-16 13:37:24 -0600

    add simple prophet forecast and gecko logo

--- src/app.py
+++ src/app.py
@@ -1,35 +1,60 @@
 import streamlit as st
 
 from coingecko import get_full_token_df, get_trending_token_df, get_cg_price, get_coin_info_dict_by_id
-from visualize import plotly_ma_daily_price
+from visualize import plotly_ma_daily_price, plotly_seasonal_decomposition, plotly_plot_forecast
+from visualize import get_gecko_logo
+from model import make_forecast_df
 
-DF_TOKENS, DF_TREND = get_full_token_df(), get_trending_token_df()
 
-max_width_str = f"max-width: 1200px;"
-st.markdown(
-    f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-    unsafe_allow_html=True,
-)
+DF_TOKENS = get_full_token_df()
+DF_TREND = get_trending_token_df()
+
+
+def set_app_layout_and_return_text_input():
+    """Create Side Pane
+    """
+    st.title('Crypto Asset Price Explorer')
+    
+    st.sidebar.title("Powered By CoinGecko!")
+    st.sidebar.image(get_gecko_logo(), use_column_width=True)
+    st.sidebar.header('**How to use:**')
+    desc_markdown = """Search for any CoinGecko listed crpto asset using its **NAME** or ticker **SYMBOL**
+    
+    
+    Examples:
+    - name: bitcoin, ethereum, chainlink, etc.
+    - symbol: sym:btc, sym:eth, sym:link, etc.
+
+    """
+    # st.sidebar.markdown(desc_markdown)
+
+    # Adjust the left margin
+    max_width_str = f"max-width: 1200px;"
+    st.markdown(f"""
+        <style>
+        .reportview-container .main .block-container{{
+            {max_width_str}
+        }}
+        </style>    
+        """,
+        unsafe_allow_html=True,
+    )
+    
+    st.header('Search for Crypto Currency by (name/ticker symbol)')
+    text_input = st.text_input('Enter a token name (bitcoin, ethereum, chainlink, etc) or a token symbol with preface sym: (sym:btc, sym:eth, etc.)', 'sym:btc')
+    return text_input
 
 
 def get_token_df_from_user_search(text_input):
-    """
+    """Get token price df from user search.
     returns: DataFrame
     """
     input = text_input.lower()
+
     if input.startswith('sym:'):
         sym = input.split('sym:')[1]
         df_token = DF_TOKENS[DF_TOKENS['symbol'] == sym]
         return df_token
-
-    # elif len(input) ==3:
-    
     df_token = DF_TOKENS[DF_TOKENS['name'].str.lower() == input]
     return df_token
 
@@ -47,19 +72,19 @@ def summarise_coin_info(resp):
         elif price_delta24hour < 0:
             msg = f'down ${price_delta24hour:,.2f}'
         st.write('In the last 24 hours price is ' + msg)
- 
+
     if sentiment:
         st.write(f'Sentiment: {sentiment}')
-    
+
     if repo:
         st.write(f'See GitHub: {repo[0]}')
+    return
+
+
 
 if __name__ == '__main__':
 
-    st.title('Crypto Price Explorer')
-
-    st.header('Search for Crypto Currency by (symbol/name/id)')
-    TEXT_INPUT = st.text_input('Enter a token name (bitcoin, ethereum, chainlink, etc) or a token symbol with preface sym: (sym:btc, sym:eth, etc.)', 'sym:btc')
+    TEXT_INPUT = set_app_layout_and_return_text_input()
     DF_TOKEN = get_token_df_from_user_search(TEXT_INPUT)
 
     TITLE = f'Retrieving CoinGecko Price Data for {DF_TOKEN["name"].iloc[0]}'
@@ -82,3 +107,13 @@ if __name__ == '__main__':
 
     fig_ma = plotly_ma_daily_price(DF, ma_slow=MA_DAYS_SLOW, ma_fast=MA_DAYS_FAST)
     st.plotly_chart(fig_ma)
+
+    if st.checkbox('Show Seasonal Decomposition'):
+        fig_decomp = plotly_seasonal_decomposition(DF['price'])
+        st.plotly_chart(fig_decomp)
+
+
+    if st.checkbox('Prophet Forecast'):
+        df_forecast = make_forecast_df(DF)
+        fig_fc = plotly_plot_forecast(df_forecast)
+        st.plotly_chart(fig_fc)
--- src/visualize.py
+++ src/visualize.py
@@ -1,9 +1,26 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 
+from plotly.subplots import make_subplots
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+from io import BytesIO
+from PIL import Image
 
 WIDTH = 1_000
 HEIGHT = 1_000
+
+
+def get_gecko_logo():
+    """Display coingecko image from static dir.
+    """
+    with open('static/gecko-mask.jpg', 'rb') as image:
+        f = image.read()
+        img = Image.open((BytesIO(f)))
+        img.convert('RGB')
+    
+    return img
 
 
 def plotly_ma_daily_price(df, ma_slow, ma_fast):
@@ -44,3 +61,72 @@ def plotly_ma_daily_price(df, ma_slow, ma_fast):
     fig.update_layout(xaxis=x_axis_dict, width=WIDTH, height=HEIGHT)
     # fig.update_layout(title='Fast and Slow Moving Average', width=WIDTH, height=HEIGHT)
     return fig
+
+
+def plotly_seasonal_decomposition(ts):
+    """
+     inputs: ts array-like, timeseries
+     returns: plotly.Figure
+    """
+    freq = 365
+
+    if len(ts) <= 750:
+        freq = 1
+
+    decomp = seasonal_decompose(ts, model='additive', period=freq)
+
+    trend = decomp.trend
+    seasonal = decomp.seasonal
+    residual = decomp.resid
+
+    fig = make_subplots(rows=2, cols=2, subplot_titles=['TimeSeries', 'Trend', 'Seasonal', 'Residual'], vertical_spacing=.05)
+
+    fig_ts = go.Scatter(name="TimeSeries", x=ts.index, y=ts, hovertemplate="%{y:$,.0f}")
+    fig_trend = go.Scatter(name="Trend", x=trend.index, y=trend, hovertemplate="%{y:$,.0f}")
+    fig_seasonal = go.Scatter(name="Seasonal", x=seasonal.index, y=seasonal, hovertemplate="%{y:$,.0f}")
+    fig_resid = go.Scatter(name="Residuals", x=residual.index, y=residual, hovertemplate="%{y:$,.0f}")
+
+    fig.add_trace(fig_ts, row=1, col=1)
+    fig.add_trace(fig_trend, row=1, col=2)
+    fig.add_trace(fig_seasonal, row=2, col=1)
+    fig.add_trace(fig_resid, row=2, col=2)
+    fig.update_layout(title=f"Seasonal Decomposition ({freq} periods)", height=HEIGHT, width=WIDTH)
+
+    return fig
+
+
+def plotly_plot_forecast(df_forecast):
+    """
+        inputs: ts array-like, timeseries
+        returns: plotly.Figure
+    """
+    fig = go.Figure()
+    # trend = go.Scatter(name='trend', mode='lines', x=df_forecast['ds'].tolist(), y=df_forecast['yhat'].tolist(), marker=dict(color='red', line=dict(width=3)))
+    lower_band = go.Scatter(name="lower_band", mode="lines", x=df_forecast["ds"].tolist(), y=df_forecast["yhat_lower"].tolist(), line= dict(color='#FF8787'), hovertemplate="%{y:$,.02f}")
+    upper_band = go.Scatter(name="upper_band", mode="lines", x=df_forecast["ds"].tolist(), y=df_forecast["yhat_upper"].tolist(), line=dict(color="#229442"), hovertemplate="%{y:$,.02f}", fill="tonexty")
+    actual_price = go.Scatter(name="Actual", mode="markers", x=df_forecast["ds"].tolist(), y=df_forecast["y"].tolist(), marker=dict(symbol='x', color="#475A61"), hovertemplate="%{y:$,.02f}")
+    price_forecast = go.Scatter(name="Forecast", mode="markers", x=df_forecast["ds"].tolist(), y=df_forecast["yhat"].tolist(), marker=dict(symbol='x', color="#5948E0"), hovertemplate="%{y:$,.02f}")
+    # fig.add_trace(trend)
+    fig.add_trace(actual_price)
+    fig.add_trace(lower_band)
+    fig.add_trace(upper_band)
+    fig.add_trace(price_forecast)
+
+    x_axis_dict = dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="ytd", step="year", stepmode="todate"),
+                dict(step="all")
+                ])
+            ),
+        rangeslider=dict(visible=True),
+        type="date",
+        zeroline=True)
+
+    fig.update_layout(xaxis=x_axis_dict, width=WIDTH, height=HEIGHT)
+    # fig.show()
+    return fig
+    
+
